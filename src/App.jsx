00c9d9147a211bf2372f90919d7668375a4ac41d
Hakan Şahin <hakonsahin@gmail.com>
2025-07-02 02:01:24 +0300

    vercel updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Categories from "./components/Categories";
 import CategoriesAndProducts from "./components/CategoriesAndProducts";
-import Header from "./components/header";
+import Header from "./components/Header";
 
 function App() {
   return (
