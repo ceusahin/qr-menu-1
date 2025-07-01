fbf90837636d1098b0cecb48f87ffd4ba416fd51
Hakan Şahin <hakonsahin@gmail.com>
2025-07-02 02:09:19 +0300

    verecl updated

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,7 @@ export const products = [
     categoryId: 2,
     name: "Margarita Pizza",
     description: "Domates soslu, mozzarella peynirli ve taze fesleğenli klasik pizza.",
-    image: "../public/pr-margarita.jpg",
+    image: "images/pr-margarita.jpg",
     price: 45.00,
 },
 {
@@ -12,14 +12,14 @@ export const products = [
     categoryId: 2,
     name: "Pepperoni Pizza",
     description: "Mozzarella peyniri ve domates soslu baharatlı pepperoni dilimleri.",
-    image: "../public/pr-pepperoni.jpeg",
+    image: "images/pr-pepperoni.jpeg",
     price: 50.00,
 },
 {    
     id: 3,
     categoryId: 2,
     name: "Vejetaryen Pizza",
-    image: "../public/pr-veg.jpg",
+    image: "images/pr-veg.jpg",
     description: "Mevsim sebzelerinin karışımı, mozzarella peyniri ve domates sosuyla servis edilir.",
     price: 50.00,
 
@@ -28,7 +28,7 @@ export const products = [
     id: 4,
     categoryId: 3,
     name: "Klasik Hamburger",
-    image: "../public/pr-klasikburger.jpg",
+    image: "images/pr-klasikburger.jpg",
     description: "Izgara dana köftesi, marul, domates, soğan ve özel sos ile taze hamburger ekmeği arasında.",
     price: 50.00,
 
@@ -36,7 +36,7 @@ export const products = [
 {    id: 5,
     categoryId: 3,
     name: "Cheeseburger",
-    image: "../public/pr-cheeseburger.jpg",
+    image: "images/pr-cheeseburger.jpg",
     description: "Izgara dana köftesi, cheddar peyniri, marul, domates, soğan ve özel sos ile taze hamburger ekmeği arasında.",
     price: 50.00,
 
@@ -44,17 +44,17 @@ export const products = [
 {    id: 6,
     categoryId: 3,
     name: "Tavuk Burger",
-    image: "../public/pr-tavukburger.jpeg",
+    image: "images/pr-tavukburger.jpeg",
     description: "Izgara tavuk göğsü, marul, domates, soğan ve özel sos ile taze hamburger ekmeği arasında.",
     price: 50.00,
 },
 ];
 
 export const categories = [
-{ id: 1, name: "Kampanyalar", image: "../public/ct-kampanya.png" },
-{ id: 2, name: "Pizza", image: "../public/ct-pizza.jpg" },
-{ id: 3, name: "Hamburger", image: "../public/ct-hamburger.jpg" },
-{ id: 4, name: "Salata", image: "../public/ct-salata.jpg" },
-{ id: 5, name: "Tatlı", image: "../public/ct-tatli.png" },
-{ id: 6, name: "İçecek", image: "../public/ct-icecek.jpg" },
+{ id: 1, name: "Kampanyalar", image: "images/ct-kampanya.png" },
+{ id: 2, name: "Pizza", image: "images/ct-pizza.jpg" },
+{ id: 3, name: "Hamburger", image: "images/ct-hamburger.jpg" },
+{ id: 4, name: "Salata", image: "images/ct-salata.jpg" },
+{ id: 5, name: "Tatlı", image: "images/ct-tatli.png" },
+{ id: 6, name: "İçecek", image: "images/ct-icecek.jpg" },
 ]
