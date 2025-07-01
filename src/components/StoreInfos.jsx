c9f78098321988039f28eb1827f24e199b4eb9bd
Hakan Şahin <hakonsahin@gmail.com>
2025-07-02 02:10:14 +0300

    vercel updated

--- src/components/StoreInfos.jsx
+++ src/components/StoreInfos.jsx
@@ -4,14 +4,14 @@ function StoreInfos() {
   return (
     <div className="relative w-full">
       <img
-        src="../public/rest-pic.jpg"
+        src="images/rest-pic.jpg"
         alt="rest-pic"
         className="w-full h-auto block"
       />
 
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <img
-          src="../public/logo-pic.png"
+          src="images/logo-pic.png"
           alt="logo"
           className="w-30 h-30 rounded-full"
         />
